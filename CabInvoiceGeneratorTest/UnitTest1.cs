272b78f44cae9a205975efd9ecf1b98663fb42b2
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-08-17 23:40:32 +0530

    [Ariprasath] added: addtion to normal ridetype, test cases are also added to check the same for premium ride type also

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -43,6 +43,40 @@ namespace CabInvoiceGeneratorTest
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
             Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
         }
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFareForPremiumTypeCab()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            double distance = 7.0;
+            int time = 20;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 145;
+            Assert.AreEqual(expected, fare);
+        }
+
+        [Test]
+        public void GivenMultipleRideShouldReturnInvoiceSummaryForPremiumTypeCab()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(4.0, 10), new Ride(0.7, 4) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(4, 42.0);
+            Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
+
+        [Test]
+        public void GivenMultipleRidewithUserIdShouldReturnInvoiceSummaryAccordingtoUserIdForPremiumTypecab()
+        {
+            RideRepository rideRepository = new RideRepository();
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
+            rideRepository.AddRide("Pawan", rides);
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rideRepository.getRides("Pawan"));
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 35.0);
+            Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
+
     }
 
 }
